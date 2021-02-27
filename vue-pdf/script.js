1d0469d9599e7bb39e887dda2d24158a0685a6d9
mdafsha9 <mdafsha9@gmail.com>
2021-02-27 18:14:36 +0530

    deleted unnecessary lines

--- vue-pdf/script.js
+++ vue-pdf/script.js
@@ -1,12 +1,7 @@
 var myObj = new Vue({
-<<<<<<< HEAD
   el:'#app',
   data:{
     message:"Hello World!"
-=======
-  el: '#app',
-  data:{
-    message:'Hello Vue!'
->>>>>>> a2d069ba0d7608d587f5fcefdc8a725bc65cc502
+
   }
 })
