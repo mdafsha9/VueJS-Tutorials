d3cc6766a13df03cc4c13aa53203112cd8e79ba9
mdafsha9 <mdafsha9@gmail.com>
2021-06-16 14:44:41 +0530

    registered local component

--- components.js
+++ components.js
@@ -4,8 +4,18 @@ Vue.component('test-component', {
       count :0
     }
   },
-  template:'<div><h4>The components which are globally registered can be used in the template of any root Vue instance(new Vue) created after registration.We have registered the components globally using Vue.component method.Global registration makes the component globally available in our application.This is the way of registering global component.</h4><button v-on:click="count++">You clicked me {{count}} times</button></div>'
+  template:'<div><h2>Registered Global component</h2><p>The components which are globally registered can be used in the template of any root Vue instance(new Vue) created after registration.Globally registered components uses Vue.component method.Global registration makes the component globally available in our application.This is the way of registering global component.</p><button v-on:click="count++">You clicked me {{count}} times</button></div>'
 })
 var vm = new Vue({
-  el:'#comp',
+  el:'#Glcomp',
+})
+
+
+new Vue({
+  el:'#LgComp',
+  components:{
+    'test-component':{
+      template:'<div><h2>Registered Local component</h2><p>Due to Global registration, even if you do not use the component, it could still included in your final build. So it will create unnecessary javascript in your application. This can be avoided using Local registration.</p></div>'
+    }
+  }
 })
