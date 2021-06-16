dbffa95d9ea46bdc1f831444de5725de844a4232
mdafsha9 <mdafsha9@gmail.com>
2021-06-16 13:34:20 +0530

    global component with click event

--- components.js
+++ components.js
@@ -1,6 +1,11 @@
 Vue.component('test-component', {
-  template:'<div><h4>This is the way of registering a global component.</h4></div>'
+  data: function(){
+    return{
+      count :0
+    }
+  },
+  template:'<div><h4>This is the way of registering global component.</h4><button v-on:click="count++">You clicked me {{count}} times</button></div>'
 })
 var vm = new Vue({
-  el:'#comp'
+  el:'#comp',
 })
