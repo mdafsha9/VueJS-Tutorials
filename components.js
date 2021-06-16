8ecf9c3b063b7f45131e7f22933f12bee3bd654d
mdafsha9 <mdafsha9@gmail.com>
2021-06-16 14:09:31 +0530

    global registration method

--- components.js
+++ components.js
@@ -4,7 +4,7 @@ Vue.component('test-component', {
       count :0
     }
   },
-  template:'<div><h4>This is the way of registering global component.</h4><button v-on:click="count++">You clicked me {{count}} times</button></div>'
+  template:'<div><h4>The components which are globally registered can be used in the template of any root Vue instance(new Vue) created after registration.We have registered the components globally using Vue.component method.Global registration makes the component globally available in our application.This is the way of registering global component.</h4><button v-on:click="count++">You clicked me {{count}} times</button></div>'
 })
 var vm = new Vue({
   el:'#comp',
